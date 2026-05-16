62262997f916a4e2f9fe0c8b4463fd71d9a6e1be
carlo <carlosmanueljimenes8@gmail.com>
2026-05-16 17:17:27 -0400

    update railway database config

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,10 @@ app.use(helmet({
 }))
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: [
+    'http://localhost:5173',
+    'https://joseantoniobobadilla.net'
+  ],
   credentials: true,
 }))
 
